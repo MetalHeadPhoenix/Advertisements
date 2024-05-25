3990e1c0dee8733f67b2a8e8bbe4867ee9d30679
MetalHeadPhoenix <ashkanhabibi80@gmail.com>
2024-05-25 23:52:00 +0330

    run app according to ENV variable

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
 from src import create_app
+from src.extensions import db
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-"""
-    Production -> config.Production
-    Development -> config.Development
-    Test -> config.Test
-"""
+
 
 if __name__ == "__main__":
-    app = create_app('config.Development')
+
+    if os.getenv('ENV') == 'PRODUCTION':
+        app = create_app('config.Production')
+
+    elif os.getenv('ENV') == 'DEVELOPMENT':
+        app = create_app('config.Development')
+
+    elif os.getenv('ENV') == 'TEST':
+        app = create_app('config.Test')
+
+        with app.app_context():
+            db.create_all()
+            print("Creating all")
     app.run(host='0.0.0.0', port=5000)
