d89feb88c203159d0a6fc26b51b5d8344866d606
MetalHeadPhoenix <ashkanhabibi80@gmail.com>
2024-05-27 23:58:11 +0330

    add comment model

--- src/models/comment.py
+++ src/models/comment.py
@@ -1,6 +1,7 @@
 from src.extensions import db
 from src.models.base import BaseModel
 from sqlalchemy import Column, String, Integer, ForeignKey
+from flask_babel import gettext
 
 
 
@@ -12,12 +13,23 @@ class CommentModel(BaseModel):
     user_id = Column(Integer, ForeignKey('users.id'))
 
     @classmethod
-    def create():
-        pass
+    def create(cls, args, advertisement_id, user_id):
+        comment = cls(**args, advertisement_id=advertisement_id, user_id=user_id)
+        # comment['advertisement_id'] = advertisement_id
+        db.session.add(comment)
+        try:
+            db.session.commit()
+            return {'msg': gettext('comment.create.success')}, 201
+        except:
+            return {'msg': gettext('error.server.internal')}, 500
 
     @classmethod
-    def read():
-        pass
+    def read(cls, advertisement_id):
+        comments = cls.query.filter_by(advertisement_id=advertisement_id)
+        result = [ row._asdict() for  row in comments]
+        if comments is not None:
+            return result
+        return {'msg':gettext('comment.not_found')}, 404
 
     @classmethod
     def update():
